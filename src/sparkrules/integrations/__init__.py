e83619bbdaaaa347e42fd2360a2e6343cfb50b13
vaquarkhan <vaquarkhan@users.noreply.github.com>
2026-05-02 14:40:27 -0500

    fix: import error in integrations - merge_features_into_fact from feast_client not tecton_client

--- src/sparkrules/integrations/__init__.py
+++ src/sparkrules/integrations/__init__.py
@@ -1,9 +1,9 @@
-from sparkrules.integrations.feast_client import FeastFeatureClient, feast_fetch_row
-from sparkrules.integrations.tecton_client import (
-    TectonFeatureClient,
+from sparkrules.integrations.feast_client import (
+    FeastFeatureClient,
+    feast_fetch_row,
     merge_features_into_fact,
-    tecton_fetch_row,
 )
+from sparkrules.integrations.tecton_client import TectonFeatureClient, tecton_fetch_row
 
 __all__ = [
     "FeastFeatureClient",
